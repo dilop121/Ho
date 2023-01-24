79e8861069d86a47683b3b68d9bfec84973dd2bd
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-25 05:16:06 +0530

    Update __init__.py

--- HowAllBot/__init__.py
+++ HowAllBot/__init__.py
@@ -1,7 +1,8 @@
+import logging
 from config import Config
 from pyrogram import Client
 from rich.console import Console
-from motor.motor_asyncio import AsyncIOMotorClient
+
 
 
 #getting variables
@@ -10,18 +11,13 @@ API_HASH = Config.API_HASH
 TOKEN = Config.BOT_TOKEN
 OWNER_ID = Config.OWNER_ID
 START_PIC = Config.START_PIC
-MONGO_DB = Config.MONGO_DB_URL
 if not START_PIC:
     START_PIC = "https://graph.org/file/c1c19fee2ac7b458087f7.jpg"
 
 #rich
 LOG = Console()
 
-#database
-mongo = AsyncIOMotorClient(MONGO_DB)
-db = mongo.HowAllBot
 
-import logging
 logging.basicConfig(level=logging.INFO)
 
 #client
