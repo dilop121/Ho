c3e75035a53d59d6777384b19594262d4883b264
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-25 04:47:53 +0530

    Update __init__.py

--- HowAllBot/__init__.py
+++ HowAllBot/__init__.py
@@ -1,1 +1,42 @@
+import os
+import time
+import asyncio
+import logging
+from config import Config
+from pyrogram import Client
+from rich.console import Console
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+#getting variables
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+TOKEN = Config.BOT_TOKEN
+OWNER_ID = Config.OWNER_ID
+START_PIC = Config.START_PIC
+MONGO_DB = Config.MONGO_DB_URL
+SUDOES.append(OWNER_ID)
+if not START_PIC:
+    START_PIC = "https://graph.org/file/c1c19fee2ac7b458087f7.jpg"
+
+#rich
+LOG = Console()
+
+#logger
+logging.basicConfig(level=logging.INFO)
+
+#database
+mongo = AsyncIOMotorClient(MONGO_DB)
+db = mongo.HowAllBot
+
+
+#client
+app = Client(
+    "SupremeStark",
+    api_id = API_ID,
+    api_hash = API_HASH,
+    bot_token = TOKEN )
+    
+
+
 
