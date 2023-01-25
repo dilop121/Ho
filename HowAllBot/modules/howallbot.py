3ae9c56ad097afab5ed5473fa05a10d542dbf2da
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-25 06:11:46 +0530

    Update howallbot.py

--- HowAllBot/modules/howallbot.py
+++ HowAllBot/modules/howallbot.py
@@ -10,12 +10,12 @@ async def _app(_, inlinequery):
     await inlinequery.answer(
     results=[
         InlineQueryResultArticle(
-        title = "💋 ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !"
-        description = "ғɪɴᴅ ᴏᴜᴛ ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !"
+        title = "💋 ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !",
+        description = "ғɪɴᴅ ᴏᴜᴛ ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !",
         input_message_content=InputTextMessageContent
           (f"💋 I ᴀᴍ {random.randint(1,100)}% ʜᴏʀɴʏ .",)
         ),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ғɪɴᴅ ᴏᴜᴛ ʏᴏᴜʀ ʀᴇsᴜʟᴛs", switch_inline_query="")]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ғɪɴᴅ ᴏᴜᴛ ʏᴏᴜʀ ʀᴇsᴜʟᴛs", switch_inline_query="")]]),
                                                                                                                                                                                                       
      ],
      cache_time=1)
