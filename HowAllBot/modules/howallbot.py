68d4deb55a6db4085a88a9fa7e42e29e0f19e644
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-25 06:08:56 +0530

    Create howallbot.py

---
+++ HowAllBot/modules/howallbot.py
@@ -0,0 +1,23 @@
+import random 
+from HowAllBot import app
+from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent,
+                            InlineKeyboardMarkup, InlineKeyboardButton)
+
+
+
+@app.on_inline_query()
+async def _app(_, inlinequery):
+    await inlinequery.answer(
+    results=[
+        InlineQueryResultArticle(
+        title = "💋 ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !"
+        description = "ғɪɴᴅ ᴏᴜᴛ ʜᴏᴡ ʜᴏʀɴʏ ʏᴏᴜ ᴀʀᴇ !"
+        input_message_content=InputTextMessageContent
+          (f"💋 I ᴀᴍ {random.randint(1,100)}% ʜᴏʀɴʏ .")
+        )
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ғɪɴᴅ ᴏᴜᴛ ʏᴏᴜʀ ʀᴇsᴜʟᴛs", switch_inline_query="")]])
+                                                                                                                                                                                                      
+     ],
+     cache_time=1)
+    
+    
