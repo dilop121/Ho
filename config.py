8fd8fc580b0e11523db125891ffaaa5475fad253
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-25 08:45:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
-    API_ID = 14817849    # don't use "" just fill your api id here 
-    API_HASH = "f72b972aadabef20c7ea193362f7c7ae" #api hash
-    BOT_TOKEN = "5589920583:AAEa8QFQbVS4njKYQ9tSh2RW1P0w1xvYrAQ" #token of your bot get it from @BotFather
-    OWNER_ID = 5505555398 #id of the owner note :- only owner can broadcast message 
+    API_ID =     # don't use "" just fill your api id here 
+    API_HASH = "" #api hash
+    BOT_TOKEN = "" #token of your bot get it from @BotFather
+    OWNER_ID = 5505555398 #id of the owner 
     START_PIC = "" #not necessary 
     
