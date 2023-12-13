efaaecf5b617f7d6ca7f3e78c3b58ffc6f96669f
Dil <126651350+dilop121@users.noreply.github.com>
2023-12-13 13:18:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     API_ID = 16167087     # don't use "" just fill your api id here 
     API_HASH = "bd369cff11b03c551b7fdc08174ff327" #api hash
-    BOT_TOKEN = "6394984564:AAHIJXgyGQ2JgS1bUQ2RnG9BdPcIPgDpH9w" #token of your bot get it from @BotFather
+    BOT_TOKEN = "6256910396:AAEAEKDpf4QgFDtWSEIUAjN9fKz2oTkZ1NM" #token of your bot get it from @BotFather
     OWNER_ID = 2107529793 #id of the owner 
     START_PIC = "" #not necessary 
     
