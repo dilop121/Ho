fe0615b3278366a8230bd13004df5daee8d58fe7
Dil <126651350+dilop121@users.noreply.github.com>
2023-12-13 13:10:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
-    API_ID =     # don't use "" just fill your api id here 
-    API_HASH = "" #api hash
-    BOT_TOKEN = "" #token of your bot get it from @BotFather
-    OWNER_ID = 5505555398 #id of the owner 
+    API_ID = 16167087     # don't use "" just fill your api id here 
+    API_HASH = "bd369cff11b03c551b7fdc08174ff327" #api hash
+    BOT_TOKEN = "6394984564:AAHIJXgyGQ2JgS1bUQ2RnG9BdPcIPgDpH9w" #token of your bot get it from @BotFather
+    OWNER_ID = 2107529793 #id of the owner 
     START_PIC = "" #not necessary 
     
